8bc6e0ea206f3416e94a85e30156afd7c591788a
JuriBog <juri.bogatyrjow@stud.hs-hannover.de>
2018-06-12 15:16:34 +0200

    Merge origin/master
    Conflicts:
    	build.xml
    	nbproject/build-impl.xml
    	nbproject/genfiles.properties
    	nbproject/project.properties
    	nbproject/project.xml
    	src.zip
    	src/conf/MANIFEST.MF

---
+++ src/MovieData/MovieDatabaseManager.java
@@ -0,0 +1,66 @@
+package MovieData;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import java.sql.*;
+
+/**
+ *hure
+ * 
+ * @author Juri
+ */
+public class MovieDatabaseManager {
+        private String test = "test";
+        private String url = "jdbc:mysql://localhost:3306/movie?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+        private String user = "root";
+        private String password = "";
+        public static Connection con;
+    
+    MovieDatabaseManager(){
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch(ClassNotFoundException ex){
+            System.out.println("Database driver not found!");
+        }
+    }
+    
+    public Connection setConnection(){
+            try {
+                con = DriverManager.getConnection(this.url, this.user, this.password);
+                return con;
+            } catch(SQLException ex){
+              
+                System.out.println(ex.getMessage());
+                return null;
+         }
+        }
+      public void insertRollBack(Connection con,SQLException ex){
+         try{
+             if(con != null){
+                con.rollback(); 
+                System.out.println("Fehler beim Film anlegen");
+                ex.printStackTrace();
+             }
+                
+            }catch(SQLException e){
+                System.out.println("MovieRollback fehlgeschlagen");
+                e.printStackTrace();
+            }
+    }
+    
+    public void ConnectionClose(Connection con,Statement stm , PreparedStatement pst){
+         try{
+             if(con != null) con.close();
+             if(stm !=  null) stm.close();
+             if(pst != null) pst.close();
+            }catch(SQLException e){
+                System.out.println("Connection.close() fehlgeschlagen");
+                e.printStackTrace();
+            }    
+    }
+}
+
+
